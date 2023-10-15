70a58b688065964928582b34e0039723e4169cf2
Clément Michaud <clement.michaud34@gmail.com>
2023-10-15 15:01:15 +0200

    add an implementation trading computation for memory
    Realizing that the weights are also sparse, it might be interesting to
    exploit this fact to allocate only what is necessary in memory. This has
    the downside of being slower in terms query but I think this is definitely
    optimizable. This was just a proof of concept.

--- Python/diadic_test.py
+++ Python/diadic_test.py
@@ -2,55 +2,71 @@
 """
 The xcount variable below sets how many records would be stored & queried within memory
 """
-xcount = 450000
-SDR_SIZE = 1000  # Size of SDRs in bits
-SDR_BITS =   10  # Default number of ON bits (aka solidity)
+import argparse
 
 from time import time
 import numpy as np
-from sdrsdm import DiadicMemory, randomSDR
+from mem_sdrsdm import DiadicMemory as MemDiadicMemory
+from sdrsdm import DiadicMemory
+from sdr_util import random_sdrs
 
-print(f"Testing DiadicMemory for {xcount} entries,\nsdr size is {SDR_SIZE} with {SDR_BITS} of ON bits")
-# The number of records to write and query
 
-t = time()
-X = randomSDR(xcount+1, N = SDR_SIZE, P = SDR_BITS)
-t = time() - t
-print(f"{xcount+1} random SDRs generated in {int(t*1000)}ms")
-    
-    
-mem = DiadicMemory(SDR_SIZE,SDR_BITS) ######################################## Initialize the memory
+SDR_SIZE = 1000  # Size of SDRs in bits
+SDR_BITS =   10  # Default number of ON bits (aka solidity)
 
-t = time()
-for i,x in enumerate(X):
-    if i == xcount:
-        break
-    y = X[i+1]
-    mem.store(x,y)   ######################################################## Store operation
-t = time() - t
-print(f"{xcount} writes in {int(t*1000)} ms")
+def test_diadic(mem, xcount):
+    print(f"Testing DiadicMemory for {xcount} entries,\nsdr size is {SDR_SIZE} with {SDR_BITS} of ON bits")
+    # The number of records to write and query
 
-print("Testing queries")
+    t = time()
+    X = random_sdrs(xcount+1, sdr_size = SDR_SIZE, on_bits = SDR_BITS)
+    t = time() - t
+    print(f"{xcount+1} random SDRs generated in {int(t*1000)}ms")
 
-size_errors = {}
-found = np.zeros((xcount,mem.P),dtype = np.uint16)
-t = time()
-    
-for i in range(xcount): 
-    qresult = mem.query(X[i]) ########################################## Query operations
-    if qresult.size == mem.P:
-        found[i] = qresult
-    else:
-        found[i] = X[i+1] # 
-        size_errors[i] = qresult
+    t = time()
+    for i,x in enumerate(X):
+        if i == xcount:
+            break
+        y = X[i+1]
+        mem.store(x,y)   ######################################################## Store operation
+    t = time() - t
+    print(f"{xcount} writes in {int(t*1000)} ms")
 
-t = time() - t
+    print("Testing queries")
 
-print(f"{xcount} queries done in {int(t*1000)}ms")
+    size_errors = {}
+    found = np.zeros((xcount,mem.P),dtype = np.uint16)
+    t = time()
 
-print("Comparing results with expectations")
-if len(size_errors): 
-    print(f"{len(size_errors)} size errors, check size_errors dictionary")
-diff = (X[1:] != found)
+    for i in range(xcount): 
+        qresult = mem.query(X[i]) ########################################## Query operations
+        if qresult.size == mem.P:
+            found[i] = qresult
+        else:
+            found[i] = X[i+1] # 
+            size_errors[i] = qresult
 
-print(f"{(diff.sum(axis=1) > 0).sum()} differences check diff array")
+    t = time() - t
+
+    print(f"{xcount} queries done in {int(t*1000)}ms")
+
+    print("Comparing results with expectations")
+    if len(size_errors): 
+        print(f"{len(size_errors)} size errors, check size_errors dictionary")
+    diff = (X[1:] != found)
+
+    print(f"{(diff.sum(axis=1) > 0).sum()} differences check diff array")
+
+parser = argparse.ArgumentParser(
+                    prog='diadic tester',
+                    description='test the diadic memory')
+parser.add_argument('-c', '--count', type=int, default=45000)
+parser.add_argument('-m', '--mem', action='store_true')
+args = parser.parse_args()
+
+if args.mem:
+    mem = MemDiadicMemory(SDR_SIZE, SDR_BITS) ######################################## Initialize the memory
+else:
+    mem = DiadicMemory(SDR_SIZE, SDR_BITS)
+
+test_diadic(mem, args.count)
